47207eb5f128301649292f94eee8633edc7358be
Ibrahim <mr.ibrahim.topchu@outlook.com>
2024-05-07 23:32:44 +0200

    mariadb startup script added

--- scripts/09_fill_files.sh
+++ scripts/09_fill_files.sh
@@ -128,9 +128,27 @@ RUN apt-get update && apt-get install -y mariadb-server
 COPY ./conf/ /etc/mysql/mariadb.conf.d/
 COPY ./tools /var/www/
 
-RUN service mysql start && mysql < /var/www/initial_db.sql && rm -f /var/www/initial_db.sql;
+RUN sed -i "s|\\${DB_ROOT}|${DB_ROOT}|g" /var/www/initial_db.sql && \
+    sed -i "s|\\${DB_NAME}|${DB_NAME}|g" /var/www/initial_db.sql && \
+    sed -i "s|\\${DB_USER}|${DB_USER}|g" /var/www/initial_db.sql && \
+    sed -i "s|\\${DB_PASSWORD}|${DB_PASSWORD}|g" /var/www/initial_db.sql
 
-CMD ["mysqld"]' > /home/$target_username/Inception/srcs/requirements/mariadb/Dockerfile
+COPY ./tools/startup.sh /usr/local/bin/startup.sh
+RUN chmod +x /usr/local/bin/startup.sh
+
+ENTRYPOINT ["/usr/local/bin/startup.sh"]' > /home/$target_username/Inception/srcs/requirements/mariadb/Dockerfile
+
+echo '#!/bin/bash
+
+service mariadb start
+
+mysql < /var/www/initial_db.sql
+
+rm -f /var/www/initial_db.sql
+
+rm -f "$0"
+
+exec mysqld' > /home/$target_username/Inception/srcs/requirements/mariadb/tools/startup.sh
 
 echo '[mysqld]
 character-set-server = utf8mb4
