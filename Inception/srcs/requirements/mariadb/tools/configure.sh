4a94350302247c20af6f73e217245e4365f12f6e
Ibrahim <mr.ibrahim.topchu@outlook.com>
2024-05-15 04:28:46 +0200

    Mariadb correction tests

--- Inception/srcs/requirements/mariadb/tools/configure.sh
+++ Inception/srcs/requirements/mariadb/tools/configure.sh
@@ -13,13 +13,23 @@ if [ ! -d "/var/lib/mysql/mysql" ]; then
     mysql_install_db --basedir=/usr --datadir=/var/lib/mysql --user=mysql > /dev/null
 fi
 
-# Configure the MySQL database
-chmod +x /tmp/configure_database.sh
-/tmp/configure_database.sh
+# Start the MySQL server
+/usr/bin/mysqld_safe --skip-networking &
+mysql_pid="$!"
+
+# Wait for MySQL to start
+while ! mysqladmin ping -h127.0.0.1 --silent; do
+    sleep 1
+done
+
+# Execute SQL commands to configure the MySQL database
+mysql -uroot -p "$MYSQL_ROOT_PASSWORD" < /tmp/configure.sql
+
 
 # Allow remote connections
 sed -i "s|skip-networking|# skip-networking|g" /etc/mysql/mariadb.conf.d/50-server.cnf
 sed -i "s|.*bind-address\s*=.*|bind-address=0.0.0.0|g" /etc/mysql/mariadb.conf.d/50-server.cnf
 
 # Start the MySQL server
-exec /usr/sbin/mysqld --user=mysql --console
+kill "$mysql_pid"
+/usr/sbin/mysqld --user=mysql --console
