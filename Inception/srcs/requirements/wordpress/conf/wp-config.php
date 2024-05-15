9467c4812af2dc0f06f1087510b635b23410f87e
Ibrahim <mr.ibrahim.topchu@outlook.com>
2024-05-15 03:54:30 +0200

    wordpress problem testing

---
+++ Inception/srcs/requirements/wordpress/conf/wp-config.php
@@ -0,0 +1,22 @@
+<?php
+define( 'DB_NAME', '${MYSQL_DATABASE}' );
+define( 'DB_USER', '${MYSQL_USER}' );
+define( 'DB_PASSWORD', '${MYSQL_PASSWORD}' );
+define( 'DB_HOST', '${MYSQL_HOST}' );
+define( 'DB_CHARSET', 'utf8' );
+define( 'DB_COLLATE', '' );
+define( 'FS_METHOD','direct' );
+define( 'AUTH_KEY',         '${AUTH_KEY}' );
+define( 'SECURE_AUTH_KEY',  '${SECURE_AUTH_KEY}' );
+define( 'LOGGED_IN_KEY',    '${LOGGED_IN_KEY}' );
+define( 'NONCE_KEY',        '${NONCE_KEY}' );
+define( 'AUTH_SALT',        '${AUTH_SALT}' );
+define( 'SECURE_AUTH_SALT', '${SECURE_AUTH_SALT}' );
+define( 'LOGGED_IN_SALT',   '${LOGGED_IN_SALT}' );
+define( 'NONCE_SALT',       '${NONCE_SALT}' );
+$table_prefix = 'wp_';
+define( 'WP_DEBUG', false );
+if ( ! defined( 'ABSPATH' ) ) {
+    define( 'ABSPATH', __DIR__ . '/' );
+}
+require_once ABSPATH . 'wp-settings.php';
--- Inception/srcs/requirements/wordpress/tools/wp-config-create.sh
+++ Inception/srcs/requirements/wordpress/tools/wp-config-create.sh
@@ -14,31 +14,10 @@ mv wp-cli.phar /usr/local/bin/wp
 
 # Check if wp-config.php already exists
 if [ ! -f "/var/www/wp-config.php" ]; then
-    # Create wp-config.php
-    cat << 'EOF' > /var/www/wp-config.php
-<?php
-define( 'DB_NAME', '${MYSQL_DATABASE}' );
-define( 'DB_USER', '${MYSQL_USER}' );
-define( 'DB_PASSWORD', '${MYSQL_PASSWORD}' );
-define( 'DB_HOST', '${MYSQL_HOST}' );
-define( 'DB_CHARSET', 'utf8' );
-define( 'DB_COLLATE', '' );
-define( 'FS_METHOD','direct' );
-define( 'AUTH_KEY',         '${AUTH_KEY}' );
-define( 'SECURE_AUTH_KEY',  '${SECURE_AUTH_KEY}' );
-define( 'LOGGED_IN_KEY',    '${LOGGED_IN_KEY}' );
-define( 'NONCE_KEY',        '${NONCE_KEY}' );
-define( 'AUTH_SALT',        '${AUTH_SALT}' );
-define( 'SECURE_AUTH_SALT', '${SECURE_AUTH_SALT}' );
-define( 'LOGGED_IN_SALT',   '${LOGGED_IN_SALT}' );
-define( 'NONCE_SALT',       '${NONCE_SALT}' );
-$table_prefix = 'wp_';
-define( 'WP_DEBUG', false );
-if ( ! defined( 'ABSPATH' ) ) {
-    define( 'ABSPATH', __DIR__ . '/' );
-}
-require_once ABSPATH . 'wp-settings.php';
-EOF
+    # place wp-config.php
+    mv /tmp/wp-config.php /var/www/wp-config.php
+else
+    rm -f /tmp/wp-config.php
 fi
 
 # Run additional WP-CLI commands
