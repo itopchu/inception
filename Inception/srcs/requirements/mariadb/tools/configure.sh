b7e3850ef7d74c68fc5461bfdb41d4acbf272098
Ibrahim <mr.ibrahim.topchu@outlook.com>
2024-05-24 17:48:36 +0200

    fu

--- Inception/srcs/requirements/mariadb/tools/configure.sh
+++ Inception/srcs/requirements/mariadb/tools/configure.sh
@@ -1,37 +1,18 @@
-#!/bin/sh
+#!bin/sh
 
-# Incase of any error exit script immediately
-set -e
+mysqld_safe &
 
-# Check if given database already exists
-if [ ! -d "/var/lib/mysql/"$MYSQL_DATABASE ]; then
-    # Create necessary tables and specify user
-	mariadb-install-db --user=mysql
+sleep 3
 
-    # Write settings into temporary file
+mariadb -u root <<EOF
+CREATE DATABASE $MYSQL_DATABASE;
+CREATE USER $MYSQL_USR@'%' IDENTIFIED BY '$MYSQL_PASSWORD';
+GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO $MYSQL_USR@'%';
+FLUSH PRIVILEGES;
+EOF
 
-    echo "CREATE DATABASE IF NOT EXISTS '$MYSQL_DATABASE';" > tmp.sql
+mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';"
 
-    # Create a MySQL user with the provided username and password, allowing connections from any host ('%')
-	echo "CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';" >> tmp.sql
+mysqladmin -u root -p$MYSQL_ROOT_PASSWORD shutdown
 
-    # Grant all privileges on all databases to the newly created user, allowing them to grant privileges to other users
-	echo "GRANT ALL PRIVILEGES ON *.* TO '$MYSQL_USER'@'%' WITH GRANT OPTION;" >> tmp.sql
-
-    # Grant all privileges on all databases to the root user, allowing them to grant privileges to other users
-	echo "GRANT ALL PRIVILEGES ON *.* TO 'root'@localhost IDENTIFIED BY '$MYSQL_ROOT_PASSWORD' WITH GRANT OPTION;" >> tmp.sql
-
-    # Flush the MySQL privileges to apply the changes made by the GRANT statements
-  	echo "FLUSH PRIVILEGES;" >> tmp.sql
-
-    # Execute SQL commands using MariaDB client
-    service mariadb start
-	mariadb < tmp.sql
-    service mariadb stop
-
-    # Remove temporary file
-    rm -f tmp.sql
-fi
-
-# Start MariaDB server daemon
-mariadbd --user=mysql
+mysqld_safe
--- Inception/srcs/requirements/wordpress/tools/wp-config-create.sh
+++ Inception/srcs/requirements/wordpress/tools/wp-config-create.sh
@@ -1,36 +1,16 @@
 #!/bin/bash
 
-if [ ! -f "/var/www/wordpress/wp-config.php" ]; then
+# wait for mysql to start
+sleep 6
 
-    # Replace placeholders in the wp-config-sample.php file with environment variables
-    # Replace unique phrase placeholders with actual secret keys
-    sed -e "s/username_here/$MYSQL_USER/g" \
-        -e "s/password_here/$MYSQL_PASSWORD/g" \
-        -e "s/localhost/$MYSQL_HOST/g" \
-        -e "s/database_name_here/$MYSQL_DATABASE/g" \
-		wordpress/wp-config-sample.php > wordpress/wp-config.php
+mv wp-config-sample.php wp-config.php && wp config set SERVER_PORT 3306 --allow-root
 
-    # Generate unique authentication keys and salts
-    wp config shuffle-salts --path=/var/www/wordpress/ > /dev/null
+wp config set DB_NAME $MYSQL_DATABASE --allow-root --path=/var/www/html
+wp config set DB_USER $MYSQL_USR --allow-root --path=/var/www/html
+wp config set DB_PASSWORD $MYSQL_PASSWORD --allow-root --path=/var/www/html
+wp config set DB_HOST $MYSQL_HOST --allow-root --path=/var/www/html
 
-    # Install WordPress
-    wp core install \
-        --allow-root \
-        --path=/var/www/wordpress/ \
-        --url="$DOMAIN_NAME" \
-        --title="$WP_TITLE" \
-        --admin_user="$WP_ADMIN_USR" \
-        --admin_password="$WP_ADMIN_PWD" \
-        --admin_email="$WP_ADMIN_EMAIL" \
-        --skip-email
-
-    # Create a new user
-    wp user create \
-        --allow-root \
-        --path=/var/www/wordpress/ \
-        "$WP_USR" \
-        "$WP_EMAIL" \
-        --user_pass="$WP_PWD"
-fi
+wp core install --url=$DOMAIN_NAME --title=$WP_TITLE --admin_user=$WP_ADMIN_USR  --admin_password=$WP_ADMIN_PWD --admin_email=$WP_ADMIN_EMAIL --allow-root --path=/var/www/html
+wp user create $WP_USR $WP_EMAIL --role=author --user_pass=$WP_PWD --allow-root --path=/var/www/html
 
 php-fpm${PHP_VERSION} -F
