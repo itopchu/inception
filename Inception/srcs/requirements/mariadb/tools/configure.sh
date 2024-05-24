ec7747f3cd7c3544ca44fcf2ffb8bff83b5ce06f
Ibrahim <mr.ibrahim.topchu@outlook.com>
2024-05-24 18:11:04 +0200

    I hate web dev

--- Inception/srcs/requirements/mariadb/tools/configure.sh
+++ Inception/srcs/requirements/mariadb/tools/configure.sh
@@ -1,18 +1,27 @@
-#!bin/sh
+#!/bin/sh
 
+# Start the MariaDB server in the background
 mysqld_safe &
 
-sleep 3
+# Wait for the MariaDB server to start
+sleep 5
 
+# Set the root password and create the database and user
 mariadb -u root <<EOF
-CREATE DATABASE $MYSQL_DATABASE;
-CREATE USER $MYSQL_USR@'%' IDENTIFIED BY '$MYSQL_PASSWORD';
-GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO $MYSQL_USR@'%';
+CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;
+CREATE USER IF NOT EXISTS '$MYSQL_USR'@'%' IDENTIFIED BY '$MYSQL_PWD';
+GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '$MYSQL_USR'@'%';
 FLUSH PRIVILEGES;
 EOF
 
-mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';"
+# Alter the root user password
+mariadb -u root <<EOF
+ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';
+FLUSH PRIVILEGES;
+EOF
 
+# Shut down the MariaDB server
 mysqladmin -u root -p$MYSQL_ROOT_PASSWORD shutdown
 
-mysqld_safe
+# Start the MariaDB server in the foreground
+exec mysqld_safe
--- Inception/srcs/requirements/wordpress/tools/wp-config-create.sh
+++ Inception/srcs/requirements/wordpress/tools/wp-config-create.sh
@@ -3,11 +3,13 @@
 # wait for mysql to start
 sleep 6
 
+cd /var/www/html && wp core download --allow-root
+
 mv wp-config-sample.php wp-config.php && wp config set SERVER_PORT 3306 --allow-root
 
 wp config set DB_NAME $MYSQL_DATABASE --allow-root --path=/var/www/html
 wp config set DB_USER $MYSQL_USR --allow-root --path=/var/www/html
-wp config set DB_PASSWORD $MYSQL_PASSWORD --allow-root --path=/var/www/html
+wp config set DB_PASSWORD $MYSQL_PWD --allow-root --path=/var/www/html
 wp config set DB_HOST $MYSQL_HOST --allow-root --path=/var/www/html
 
 wp core install --url=$DOMAIN_NAME --title=$WP_TITLE --admin_user=$WP_ADMIN_USR  --admin_password=$WP_ADMIN_PWD --admin_email=$WP_ADMIN_EMAIL --allow-root --path=/var/www/html
