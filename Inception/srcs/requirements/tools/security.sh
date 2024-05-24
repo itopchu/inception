261e3d79c7edff9c4995b3dd84f9a09ba8faac89
Ibrahim <mr.ibrahim.topchu@outlook.com>
2024-05-24 02:18:55 +0200

    update for security.sh

--- Inception/srcs/requirements/tools/security.sh
+++ Inception/srcs/requirements/tools/security.sh
@@ -3,6 +3,9 @@
 # This script adds security keys to .env file to make it reachable by docker-compose
 # It also protects old values
 
+username="${1:-itopchu}"
+env_path=/home/$username/Inception/srcs/.env
+
 # Get secret keys from wordpress api
 curl -s https://api.wordpress.org/secret-key/1.1/salt/ -o keys.txt
 
@@ -13,15 +16,16 @@ while IFS= read -r line; do
     var_value=$(echo "$line" | sed -E "s/define\('[^']*',\s*'([^']*)'\);/\1/")
 
     # Check if variable name already exists in the .env file
-    if grep -q "^$var_name=" ../../.env && grep -q "^$var_name=." ../../.env; then
+    if grep -q "^$var_name=" env_path && grep -q "^$var_name=." env_path; then
         continue
     fi
+
     # Remove any existing variable from .env file
-    sed -i "/^$var_name/d" ../../.env
+    sed -i "/^$var_name/d" env_path
 
     # Append the variable and its value to the .env file if there's an '=' and non-empty value
     if [ -n "$var_value" ]; then
-        echo "$var_name='$var_value'" >> ../../.env
+        echo "$var_name='$var_value'" >> env_path
     fi
 done < keys.txt
 
