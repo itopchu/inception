a675219085df42ab75bf264c872e61d155059a70
Ibrahim <mr.ibrahim.topchu@outlook.com>
2024-05-24 13:41:25 +0200

    environment variable handling corrected

--- Inception/srcs/requirements/wordpress/tools/wp-config-create.sh
+++ Inception/srcs/requirements/wordpress/tools/wp-config-create.sh
@@ -3,20 +3,19 @@
 if [ ! -f "/var/www/wordpress/wp-config.php" ]; then
 
     # Replace placeholders in the wp-config-sample.php file with environment variables
-	sed -i "s/username_here/$MYSQL_USER/g" wordpress/wp-config-sample.php
-	sed -i "s/password_here/$MYSQL_PASSWORD/g" wordpress/wp-config-sample.php
-	sed -i "s/localhost/$MYSQL_HOST/g" wordpress/wp-config-sample.php
-	sed -i "s/database_name_here/$MYSQL_DATABASE/g" wordpress/wp-config-sample.php
-
     # Replace unique phrase placeholders with actual secret keys
-    sed -i "s/put your unique phrase here/$AUTH_KEY/g" wordpress/wp-config-sample.php
-    sed -i "s/put your unique phrase here/$SECURE_AUTH_KEY/g" wordpress/wp-config-sample.php
-    sed -i "s/put your unique phrase here/$LOGGED_IN_KEY/g" wordpress/wp-config-sample.php
-    sed -i "s/put your unique phrase here/$NONCE_KEY/g" wordpress/wp-config-sample.php
-    sed -i "s/put your unique phrase here/$AUTH_SALT/g" wordpress/wp-config-sample.php
-    sed -i "s/put your unique phrase here/$SECURE_AUTH_SALT/g" wordpress/wp-config-sample.php
-    sed -i "s/put your unique phrase here/$LOGGED_IN_SALT/g" wordpress/wp-config-sample.php
-    sed -i "s/put your unique phrase here/$NONCE_SALT/g" wordpress/wp-config-sample.php
+	sed -i'' -e "s/username_here/$MYSQL_USER/g" \
+			-e "s/password_here/$MYSQL_PASSWORD/g" \
+			-e "s/localhost/$MYSQL_HOST/g" \
+			-e "s/database_name_here/$MYSQL_DATABASE/g" \
+			-e "s/define( 'AUTH_KEY',         'put your unique phrase here' )/define( 'AUTH_KEY',         '$AUTH_KEY' )/g" \
+			-e "s/define( 'SECURE_AUTH_KEY',  'put your unique phrase here' )/define( 'SECURE_AUTH_KEY',  '$SECURE_AUTH_KEY' )/g" \
+			-e "s/define( 'LOGGED_IN_KEY',    'put your unique phrase here' )/define( 'LOGGED_IN_KEY',    '$LOGGED_IN_KEY' )/g" \
+			-e "s/define( 'NONCE_KEY',        'put your unique phrase here' )/define( 'NONCE_KEY',        '$NONCE_KEY' )/g" \
+			-e "s/define( 'AUTH_SALT',        'put your unique phrase here' )/define( 'AUTH_SALT',        '$AUTH_SALT' )/g" \
+			-e "s/define( 'SECURE_AUTH_SALT', 'put your unique phrase here' )/define( 'SECURE_AUTH_SALT', '$SECURE_AUTH_SAlT' )/g" \
+			-e "s/define( 'LOGGED_IN_SALT',   'put your unique phrase here' )/define( 'LOGGED_IN_SALT',   '$LOGGED_IN_SALT' )/g" \
+			-e "s/define( 'NONCE_SALT',       'put your unique phrase here' )/define( 'NONCE_SALT',       '$NONCE_SALT' )/g" wordpress/wp-config-sample.php
 
     # Copy the modified wp-config-sample.php to wp-config.php
     cp wordpress/wp-config-sample.php wordpress/wp-config.php
