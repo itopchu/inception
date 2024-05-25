2e6468f71c446b4281abe43f80fea7152e065154
Ibrahim <mr.ibrahim.topchu@outlook.com>
2024-05-25 15:52:39 +0200

    hatred

--- Inception/srcs/requirements/mariadb/tools/configure.sh
+++ Inception/srcs/requirements/mariadb/tools/configure.sh
@@ -1,20 +1,57 @@
 #!/bin/bash
 
-set -e
+# Initialize the database if it doesn't exist
+if [ ! -d "/var/lib/mysql/mysql" ]; then
 
-if [ ! -d "/var/lib/mysql/"$MYSQL_NAME"" ]; then
-	mariadb-install-db --user=mysql
+    chown -R mysql:mysql /var/lib/mysql
 
-	echo "CREATE USER '$MYSQL_USR'@'%' IDENTIFIED BY '$MYSQL_PWD';" >> php_data.sql
-	echo "GRANT ALL PRIVILEGES ON *.* TO '$MYSQL_USR'@'%' WITH GRANT OPTION;" >> php_data.sql
-	echo "GRANT ALL PRIVILEGES ON *.* TO 'root'@localhost IDENTIFIED BY '$MYSQL_ROOT_PASSWORD' WITH GRANT OPTION;" >> php_data.sql
-    # why the fuck named wordpress?
-	echo "CREATE DATABASE IF NOT EXISTS wordpress;" >> php_data.sql
-  	echo "FLUSH PRIVILEGES;" >> php_data.sql
+    # Initialize the database
+    mysql_install_db --basedir=/usr --datadir=/var/lib/mysql --user=mysql --rpm
 
-	service mariadb start
-	mariadb < php_data.sql
-	service mariadb stop
+    tfile=`mktemp`
+    if [ ! -f "$tfile" ]; then
+        return 1
+    fi
 fi
 
-mariadbd --user=mysql
+# Create the WordPress database and user if not already created
+if [ ! -d "/var/lib/mysql/$MYSQL_NAME" ]; then
+
+    cat << EOF > /tmp/create_db.sql
+USE mysql;
+FLUSH PRIVILEGES;
+DELETE FROM mysql.user WHERE User='';
+DROP DATABASE IF EXISTS test;
+DELETE FROM mysql.db WHERE Db='test';
+DELETE FROM mysql.user WHERE User='${MYSQL_ROOT}' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
+ALTER USER '${MYSQL_ROOT}'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
+CREATE DATABASE ${MYSQL_NAME} CHARACTER SET utf8 COLLATE utf8_general_ci;
+CREATE USER '${MYSQL_USR}'@'%' IDENTIFIED BY '${MYSQL_PWD}';
+GRANT ALL PRIVILEGES ON ${MYSQL_NAME}.* TO '${MYSQL_USR}'@'%';
+FLUSH PRIVILEGES;
+
+USE ${MYSQL_NAME};
+
+-- Create the administrator user
+INSERT INTO wp_users (user_login, user_pass, user_nicename, user_email, user_status)
+VALUES ('${WP_ADMIN_USR}', MD5('${WP_ADMIN_PWD}'), 'Admin User', '${WP_ADMIN_EMAIL}', 0)
+ON DUPLICATE KEY UPDATE user_pass = MD5('${WP_ADMIN_PWD}'), user_email = '${WP_ADMIN_EMAIL}';
+INSERT INTO wp_usermeta (user_id, meta_key, meta_value)
+SELECT ID, 'wp_capabilities', 'a:1:{s:13:"administrator";s:1:"1";}' FROM wp_users WHERE user_login = '${WP_ADMIN_USR}';
+
+-- Create the second user
+INSERT INTO wp_users (user_login, user_pass, user_nicename, user_email, user_status)
+VALUES ('${WP_USR}', MD5('${WP_PWD}'), 'Second User', '${WP_EMAIL}', 0)
+ON DUPLICATE KEY UPDATE user_pass = MD5('${WP_PWD}'), user_email = '${WP_EMAIL}';
+INSERT INTO wp_usermeta (user_id, meta_key, meta_value)
+SELECT ID, 'wp_capabilities', 'a:1:{s:10:"subscriber";s:1:"1";}' FROM wp_users WHERE user_login = '${WP_USR}';
+
+EOF
+
+    # Run the SQL script to initialize the database
+    /usr/bin/mysqld --defaults-file=/etc/mysql/my.cnf --user=mysql --bootstrap < /tmp/create_db.sql
+    rm -f /tmp/create_db.sql
+fi
+
+# Start the MariaDB service normally
+exec /usr/bin/mysqld --defaults-file=/etc/mysql/my.cnf --user=mysql --console
--- Inception/srcs/requirements/wordpress/tools/wp-config-create.sh
+++ Inception/srcs/requirements/wordpress/tools/wp-config-create.sh
@@ -1,33 +1,33 @@
 #!/bin/bash
 
-cd /var/www/html && wp core download --allow-root
-
 if [ ! -f "/var/www/wordpress/wp-config.php" ]; then
 
-# cd /var/www
+    # Replace placeholders in wp-config-sample.php and create wp-config.php
+    sed -i "s/database_name_here/$MYSQL_NAME/g" /var/www/wordpress/wp-config-sample.php
+    sed -i "s/username_here/$MYSQL_USR/g" /var/www/wordpress/wp-config-sample.php
+    sed -i "s/password_here/$MYSQL_PWD/g" /var/www/wordpress/wp-config-sample.php
+    sed -i "s/localhost/$MYSQL_HOST/g" /var/www/wordpress/wp-config-sample.php
+    cp /var/www/wordpress/wp-config-sample.php /var/www/wordpress/wp-config.php
 
-sed -i "s/username_here/$MYSQL_USR/g" wordpress/wp-config-sample.php
-sed -i "s/password_here/$MYSQL_PWD/g" wordpress/wp-config-sample.php
-sed -i "s/localhost/$MYSQL_HOST/g" wordpress/wp-config-sample.php
-sed -i "s/database_name_here/$MYSQL_NAME/g" wordpress/wp-config-sample.php
-cp wordpress/wp-config-sample.php wordpress/wp-config.php
+    # Install WordPress using wp-cli
+    wp core install \
+        --allow-root \
+        --path=/var/www/wordpress/ \
+        --url="$DOMAIN_NAME" \
+        --title="$WP_TITLE" \
+        --admin_user="$WP_ADMIN_USR" \
+        --admin_password="$WP_ADMIN_PWD" \
+        --admin_email="$WP_ADMIN_EMAIL" \
+        --skip-email
 
-wp core install \
-	--allow-root \
-	--path=/var/www/wordpress/ \
-	--url="$DOMAIN_NAME" \
-	--title="$WP_TITLE" \
-	--admin_user="$WP_ADMIN_USR" \
-	--admin_password="$WP_ADMIN_PWD" \
-	--admin_email="$WP_ADMIN_EMAIL" \
-	--skip-email \
-
-wp user create \
-	--allow-root \
-	--path=/var/www/wordpress/ \
-	"$WP_USR" \
-	"$WP_EMAIL" \
-	--user_pass="$WP_PWD"
+    # Create a second WordPress user
+    wp user create \
+        --allow-root \
+        --path=/var/www/wordpress/ \
+        "$WP_USR" \
+        "$WP_EMAIL" \
+        --user_pass="$WP_PWD"
 fi
 
+# Start PHP-FPM
 php-fpm${PHP_VERSION} -F
