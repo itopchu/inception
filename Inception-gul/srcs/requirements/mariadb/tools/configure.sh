ec1241fa38180a3e487764d1e9e94c7b801311d7
Ibrahim <mr.ibrahim.topchu@outlook.com>
2024-05-26 15:12:28 +0200

    wololoooo

--- Inception-gul/srcs/requirements/mariadb/tools/configure.sh
+++ Inception-gul/srcs/requirements/mariadb/tools/configure.sh
@@ -3,13 +3,12 @@
 set -e
 
 if [ ! -d "/var/lib/mysql/$DB_NAME" ]; then
-	echo "Mariadb is starting"
 	mariadb-install-db --user=mysql
 	
 	echo "CREATE USER '$DB_USER'@'%' IDENTIFIED BY '$DB_USER_PWD';" >> /tmp/php_data.sql
 	echo "GRANT ALL PRIVILEGES ON *.* TO '$DB_USER'@'%' WITH GRANT OPTION;" >> /tmp/php_data.sql
 	echo "GRANT ALL PRIVILEGES ON *.* TO 'root'@localhost IDENTIFIED BY '$DB_ROOT_PWD' WITH GRANT OPTION;" >> /tmp/php_data.sql
-	echo "CREATE DATABASE IF NOT EXISTS wordpress;" >> /tmp/php_data.sql
+	echo "CREATE DATABASE IF NOT EXISTS $DB_NAME;" >> /tmp/php_data.sql
   	echo "FLUSH PRIVILEGES;" >> /tmp/php_data.sql
 
 	service mariadb start
