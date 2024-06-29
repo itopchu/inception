e08012c4a6a12f5ca9936d6acd0eb221b07893f4
Ibrahim Topchu <mr.ibrahim.topchu@outlook.com>
2024-06-29 16:22:01 +0200

    env adaptation

--- Inception/srcs/requirements/wordpress/tools/wp-setup.sh
+++ Inception/srcs/requirements/wordpress/tools/wp-setup.sh
@@ -9,33 +9,33 @@ if [ -e "/var/www/html/wp-config.php" ]; then
     exec "$@"
 fi
 
-mkdir -p "$WP_PATH"
+mkdir -p "/var/www/html"
 wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar;
 chmod +x wp-cli.phar; 
 mv wp-cli.phar /usr/local/bin/wp;
-cd "$WP_PATH";
+cd "/var/www/html";
 
 wp core download --allow-root \
 
 wp config create --allow-root \
-                --dbname=$DB_NAME \
-                --dbuser=$DB_USER \
-                --dbpass=$DB_PASSWORD \
-                --dbhost=$DB_HOST \
-                --path=$WP_PATH
+                --dbname=$MYSQL_DATABASE \
+                --dbuser=$WORDPRESS_DB_USER \
+                --dbpass=$MYSQL_ROOT_PASSWORD \
+                --dbhost=$WORDPRESS_DB_HOST \
+                --path=/var/www/html
 
 wp core install --allow-root \
-               --url="$WP_URL" \
+               --url="$DOMAIN_NAME" \
                --title="$WP_TITLE" \
-               --admin_user="$WP_ADMIN_LOGIN" \
-               --admin_password="$WP_ADMIN_PASSWORD" \
+               --admin_user="$WP_ADMIN" \
+               --admin_password="$WP_ADMIN_PWD" \
                --admin_email="$WP_ADMIN_EMAIL" \
 
 wp user create --allow-root \
-                "$WP_USER_LOGIN" \
+                "$WP_USER" \
                 "$WP_USER_EMAIL" \
                 --role=author \
-                --user_pass="$WP_USER_PASSWORD" \
+                --user_pass="$WP_USER_PWD" \
 
 chown -R www-data:www-data /var/www/html
 
