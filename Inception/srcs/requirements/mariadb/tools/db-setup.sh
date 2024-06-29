e1f36a63bceb30567f02047da3c2a07f5f23fc48
Ibrahim Topchu <itopchu@f0r4s6.codam.nl>
2024-06-29 20:42:43 +0200

    i give up

--- Inception/srcs/requirements/mariadb/tools/db-setup.sh
+++ Inception/srcs/requirements/mariadb/tools/db-setup.sh
@@ -4,37 +4,36 @@ sleep 5
 
 echo "Configuring MariaDB..."
 
-if [ ! -d "/var/lib/mysql/$DB_NAME" ]; then
+if [ ! -d "/var/lib/mysql/$DB_NAME" ];then
+	service mariadb start
 
-    service mariadb start || systemctl start mariadb
+	sleep 1
 
-    sleep 1
+	mysql_secure_installation <<- EOF
 
-    mysql_secure_installation <<EOF
-Y
-$DB_ROOT_PWD
-$DB_ROOT_PWD
-Y
-Y
-Y
-Y
-EOF
+	Y
+	$DB_ROOT_PASSWORD
+	$DB_ROOT_PASSWORD
+	Y
+	Y
+	Y
+	Y
+	EOF
 
-    echo "MariaDB configured"
-    echo "Creating Database..."
+	echo "MariaDB configured"
+	echo "Creating Database..."
 
-    mysql -u root -p"$DB_ROOT_PWD" <<EOF
-CREATE DATABASE IF NOT EXISTS $DB_NAME;
-CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_USER_PWD';
-GRANT ALL PRIVILEGES ON *.* TO '$DB_USER'@'%';
-FLUSH PRIVILEGES;
-ALTER USER 'root'@'localhost' IDENTIFIED BY '$DB_ROOT_PWD';
-FLUSH PRIVILEGES;
-EOF
+	mysql -u root <<- EOF
+		CREATE DATABASE IF NOT EXISTS $DB_NAME;
+		CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_PASSWORD';
+		GRANT ALL PRIVILEGES ON *.* TO '$DB_USER'@'%';
+		FLUSH PRIVILEGES;
+		ALTER USER 'root'@'localhost' IDENTIFIED BY '$DB_ROOT_PASSWORD';
+		FLUSH PRIVILEGES;
+	EOF
 
-    mysqladmin -u root -p"$DB_ROOT_PWD" shutdown
-    
-    echo "Database created"
+ 	mysqladmin -u root -p$DB_ROOT_PASSWORD shutdown
+	echo "Database created"
 
 fi
 
--- Inception/srcs/requirements/wordpress/tools/wp-setup.sh
+++ Inception/srcs/requirements/wordpress/tools/wp-setup.sh
@@ -4,18 +4,19 @@ sleep 10
 
 echo "Setting up WordPress"
 
-if [ -e "/var/www/html/wp-config.php" ]; then
+if [ -e "/var/www/html/wp-config.php" ];then
     echo "WordPress already installed. Executing server"
     exec "$@"
 fi
 
+mkdir -p "$WP_PATH"
 wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar;
 chmod +x wp-cli.phar; 
 mv wp-cli.phar /usr/local/bin/wp;
+cd "$WP_PATH";
 
-mkdir -p "$WP_PATH" && cd "$WP_PATH"
+wp core download --allow-root \
 
-wp core download --allow-root
 wp config create --allow-root \
                 --dbname=$DB_NAME \
                 --dbuser=$DB_USER \
@@ -31,9 +32,10 @@ wp core install --allow-root \
                 --path="$WP_PATH"
 wp user create --allow-root "$WP_USER_LOGIN" "$WP_USER_EMAIL" \
                 --role=author \
-                --user_pass="$WP_USER_PWD"
+                --user_pass="$WP_USER_PWD" \
                 --path="$WP_PATH"
-wp config shuffle-salts --allow-root
+wp config shuffle-salts --allow-root \
+
 chown -R www-data:www-data ${WP_PATH}
 echo "WordPress installation complete"
 exec "$@"
